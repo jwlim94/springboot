aee8a765dfcd3a3449c6b5a76675f19a59eac633
jwlim94 <limjongwoo1994@gmail.com>
2021-07-17 14:13:29 -0600

    finish using custom annotation

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,8 +1,8 @@
 package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.config.auth.LoginUser;
 import org.example.springboot.config.auth.dto.SessionUser;
-import org.example.springboot.domain.user.User;
 import org.example.springboot.service.posts.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.mail.Session;
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
@@ -18,15 +17,13 @@ import javax.servlet.http.HttpSession;
 public class IndexController { // PURPOSE: 페이지간의 이동 API
 
   private final PostsService postsService;
-  private final HttpSession httpSession;
 
   @GetMapping("/")
-  public String index(Model model) {
+  public String index(Model model, @LoginUser SessionUser user) {
     //PURPOSE: Model; 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
     //PURPOSE: 여기서는 postsService.findAllDesc()로 가져온 결과를 posts 로 index.mustache 에 전달한다.
     model.addAttribute("posts", postsService.findAllDesc());
 
-    SessionUser user = (SessionUser) httpSession.getAttribute("user");
     if (user != null) {
       model.addAttribute("loginUserName", user.getName());
     }
