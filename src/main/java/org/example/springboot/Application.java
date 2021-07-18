8c54e81623f380cf62b8a4ae82c2b48817c7acf7
jwlim94 <limjongwoo1994@gmail.com>
2021-07-18 17:19:14 -0600

    finish applying spring security to pass all tests

--- src/main/java/org/example/springboot/Application.java
+++ src/main/java/org/example/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing NOTE: HelloControllerTest 에서 WebMvcTest 를 사용하기 위해 @SpringBootApplication 과 @EnableJpaAuditing 을 분리한다.
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
--- src/test/java/org/example/springboot/web/HelloControllerTest.java
+++ src/test/java/org/example/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package org.example.springboot.web;
 
+import org.example.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,12 +18,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+  @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class HelloControllerTest { // API test
 
   @Autowired
   private MockMvc mvc;
 
+  @WithMockUser(roles="USER")
   @Test
   public void returnHello() throws Exception {
     String hello = "hello";
@@ -29,6 +36,7 @@ public class HelloControllerTest { // API test
       .andExpect(content().string(hello));
   }
 
+  @WithMockUser(roles="USER")
   @Test
   public void returnHelloDto() throws Exception {
     String name = "hello";
